11a9dbe817ba26c68cae66932ddc9c9e8c3d8d01
hendrencustomhomes <admin@hendrencustomhomes.com>
2026-04-18 15:00:47 -0500

    Add missing migration for handle_new_user fix

---
+++ supabase/migrations/20260418031431_fix_handle_new_user_full_name_default.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+RETURNS trigger
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+BEGIN
+  INSERT INTO public.profiles (id, email, full_name)
+  VALUES (new.id, new.email, '');
+  RETURN new;
+END;
+$$;
