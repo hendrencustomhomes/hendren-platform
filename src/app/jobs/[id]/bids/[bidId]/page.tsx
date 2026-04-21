0620a2360de5bb26072baa073548d05fc168ea77
hendrencustomhomes <admin@hendrencustomhomes.com>
2026-04-21 14:32:29 -0500

    Replace bid detail redirect with shared worksheet page

--- src/app/jobs/[id]/bids/[bidId]/page.tsx
+++ src/app/jobs/[id]/bids/[bidId]/page.tsx
@@ -1,6 +1,15 @@
-import { redirect } from 'next/navigation'
+import PricingWorksheetPage from '@/components/pricing/PricingWorksheetPage'
 
 export default async function JobBidDetailPage({ params }: { params: Promise<{ id: string; bidId: string }> }) {
-  const { bidId } = await params
-  redirect(`/more/price-sheets/${bidId}`)
+  const { id, bidId } = await params
+
+  return (
+    <PricingWorksheetPage
+      headerId={bidId}
+      backHref={`/jobs/${id}?tab=bids`}
+      detailBasePath={`/jobs/${id}/bids`}
+      navFallbackTitle="Bid"
+      missingLabel="Bid"
+    />
+  )
 }
